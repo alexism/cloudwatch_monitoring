2d749f02e0de0c1b297ad1606176fb32beb8cbab
wilkosz <joshua@wilkosz.com.au>
2020-04-22 21:20:58 +1000

    pop

--- libraries/iam_role.rb
+++ libraries/iam_role.rb
@@ -5,22 +5,27 @@ module IAM
     require 'net/http'
     require 'uri'
 
-    uri = URI('http://169.254.169.254/latest/api/token/')
-    req = Net::HTTP::Put.new(uri)
-    req['X-aws-ec2-metadata-token-ttl-seconds'] = '60'
-    res = Net::HTTP.start(uri.hostname, uri.port) {|http|
-      http.request(req)
-    }
-    token = res&.body
+    begin
+      uri = URI('http://169.254.169.254/latest/api/token/')
+      req = Net::HTTP::Put.new(uri)
+      req['X-aws-ec2-metadata-token-ttl-seconds'] = '60'
+      res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+        http.request(req)
+      }
+      token = res&.body
 
-    return unless token
+      return unless token
 
-    uri = URI('http://169.254.169.254/latest/meta-data/iam/security-credentials/')
-    req = Net::HTTP::Get.new(uri)
-    req['X-aws-ec2-metadata-token'] = token
-    res = Net::HTTP.start(uri.hostname, uri.port) {|http|
-      http.request(req)
-    }
-    res&.body
+      uri = URI('http://169.254.169.254/latest/meta-data/iam/security-credentials/')
+      req = Net::HTTP::Get.new(uri)
+      req['X-aws-ec2-metadata-token'] = token
+      res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+        http.request(req)
+      }
+      res&.body
+    rescue => e
+      nil
+    end
+    
   end
 end
